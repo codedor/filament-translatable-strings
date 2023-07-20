9e3c3e67be718c76aaeada1c2dfc80f458ca1dbf
jyrkidn <jyrkidn@users.noreply.github.com>
2023-07-20 10:41:25 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -6,7 +6,7 @@ if (! function_exists('__html')) {
     /**
      * Translate the given html message.
      */
-    function __html(?string $key, array $replace = [], ?string $locale = null): HtmlString
+    function __html(?string $key, array $replace = [], string $locale = null): HtmlString
     {
         return new HtmlString(__($key, $replace, $locale));
     }
